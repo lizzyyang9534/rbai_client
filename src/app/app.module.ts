aa16ec5bf54a34846a74bbf3e43a295f7144b220
LizzyYang <lizzyyang9534@yahoo.com.tw>
2016-12-15 00:32:19 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule }  from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsListComponent } from './news-list/news-list.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { LoginComponent } from './login/login.component';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NewsListComponent,
+    CarouselComponent,
+    LoginComponent,
+    CardComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/card/card.js
@@ -0,0 +1,13 @@
+
+
+    $(document).ready(function () {
+       rotateCard =  function(btn){
+              var $card = $(btn).closest('.card-container');
+              console.log($card);
+              if($card.hasClass('hover')){
+                  $card.removeClass('hover');
+              } else {
+                  $card.addClass('hover');
+              }
+          }
+    });
